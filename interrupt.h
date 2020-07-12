127a80117a042946ca59ca6bb5018be0040a434c
seokhyeon99 <lsh9058@naver.com>
2020-07-13 02:39:15 +0900

    Create interrupt.h

---
+++ interrupt.h
@@ -0,0 +1,14 @@
+#ifndef __INTERRUPT_H_
+#define __INTERRUPT_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "RoboCAR.h"
+
+
+void Timer0_Init();
+void Timer1_PWM_Init();
+
+
+#endif
